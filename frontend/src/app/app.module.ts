31d4e9d6d05b96ddd3f9d49a69108766956dcf62
faizanahmadkhan786 <faizanahmadk699@gmail.com>
2024-06-28 01:47:22 +0530

    page is completed upto tags

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partial/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/partial/search/search.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { TagsComponent } from './components/partial/tags/tags.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    FoodPageComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/services/food.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { sample_foods, sample_tags } from 'src/data';
+import { Food } from '../shared/modals/food';
+import { Tag } from '../shared/modals/Tag';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+  getAll():Food[]
+  {
+    return sample_foods;
+  }
+  getAllFoodsBySearchTerm(searchTerm:String){
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+ getFoodById(foodId:string):Food{
+ return this.getAll().find(food=>food.id==foodId)??new Food();
+ }
+ getAlltags():Tag[]{
+  return sample_tags;
+ }
+ getAllFoodByTag(tag:string):Food[]
+ {
+  return tag==="All"?
+  this.getAll():
+  this.getAll().filter(food => food.tags?.includes(tag));
+ }
+}
